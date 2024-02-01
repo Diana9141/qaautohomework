2cdac7b33f021de9457c0784cd224c59fbd4cae8
diana <diana.braun@flexmade.com>
2024-02-01 19:55:48 +0200

    added YakabooTest

--- src/main/java/hw11/YakabooTest.java
+++ src/main/java/hw11/YakabooTest.java
@@ -16,6 +16,8 @@ public class YakabooTest {
     private final SelenideElement bookType = $("div.format__text");
     private final SelenideElement bookPrice = $x("//div[@class=\"ui-price-display price simple selected\"]");
     private final SelenideElement availabilityButton = $x("(//button[@class=\"ui-btn-accept waiting-list button-accept\"])[1]");
+    private final SelenideElement bookTypeSelected = $x("//div[@class='ui-btn-format option-button selected']/*[1]/*[2]");
+    private final SelenideElement buttonFormat2 = $("div.option-list > div:nth-child(2)");
 
     @BeforeMethod()
     public static void setUp() {
@@ -26,15 +28,17 @@ public class YakabooTest {
 
     @Test()
     void searchBook() {
-        String searchText = "Скотний двір";
+        String searchText = "Pride And Prejudice";
         searchInput.setValue(searchText).pressEnter();
         firstBookFromSearch.should(visible).click();
         marketingBanner.click();
 
-        if (bookPrice.isDisplayed() && bookType.text().equals("Паперова") && !(availabilityButton.exists())) {
+        if (bookTypeSelected.text().trim().equals("Паперова") && !(availabilityButton.exists())) {
             System.out.println("Paper book is there: " + bookPrice.text());
-        } else if (bookPrice.isDisplayed() && bookType.text().equals("Електронна") && !(availabilityButton.exists())) {
+        } else if (bookTypeSelected.text().trim().equals("Електронна") && !(availabilityButton.exists())) {
             System.out.println("Electronic book is there: " + bookPrice.text());
+        } else if (bookTypeSelected.text().trim().equals("Паперова") && bookTypeSelected.text().trim().equals("Електронна") && !(availabilityButton.exists())) {
+            System.out.println("Paper and Electronic books are both available");
         } else {
             System.out.println("BOOK IS NOT AVAILABLE");
         }
