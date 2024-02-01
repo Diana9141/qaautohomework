79182391ee50c61337f4350bd2e1c4c4bb8cf86c
diana <diana.braun@flexmade.com>
2024-02-01 14:35:29 +0200

    added YakabooTest

--- src/main/java/hw11/YakabooTest.java
+++ src/main/java/hw11/YakabooTest.java
@@ -5,9 +5,9 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class YakabooTest {
     private final SelenideElement searchInput = $("input[type=\"search\"]");
@@ -15,7 +15,7 @@ public class YakabooTest {
     private final SelenideElement firstBookFromSearch = $x("(//a[@data-testid=\"productLink\"])[1]");
     private final SelenideElement paperBookPrice = $("div[class=\"ui-btn-format option-button selected\"]:first-child > div:nth-child(2)");
     private final SelenideElement electronicBookPrice = $("div[class=\"ui-btn-format option-button selected\"]:nth-child(2)");
-    public SelenideElement availabilityText = $x("(//span[@class=\"ui-shipment-status__text\"])[1]");
+    private final SelenideElement availabilityButton = $x("(//button[@class=\"ui-btn-accept waiting-list button-accept\"])[1]");
 
     @BeforeMethod()
     public static void setUp() {
@@ -25,21 +25,22 @@ public class YakabooTest {
     }
 
     @Test()
-    void searchBook() throws InterruptedException {
-        String searchText = "Мати";
+    void searchBook() {
+        String searchText = "Мати і не мати";
         searchInput.setValue(searchText).pressEnter();
-        firstBookFromSearch.should(appear).click();
-        marketingBanner.isDisplayed();
+        firstBookFromSearch.should(visible).click();
+        marketingBanner.click();
 
-        if (!availabilityText.text().trim().equals("Немає в наявності") && paperBookPrice.exists()) {
-            System.out.println("Паперова версія доступна. Ціна: " + paperBookPrice.text());
-        } else if (electronicBookPrice.exists() && !availabilityText.text().trim().equals("Немає в наявності")) {
-            System.out.println("Електронна версія доступна. Ціна: " + electronicBookPrice.text());
-        } else {
-            System.out.println("Книга не в наявності.");
+        if(paperBookPrice.isDisplayed() && !(availabilityButton.exists())) {
+            System.out.println("Paper book is there");
+        } else if (electronicBookPrice.isDisplayed() && !(availabilityButton.exists())) {
+            System.out.println("Electronic book is there");
         }
-        Thread.sleep(5000);
+        else {
+            System.out.println("NOT THERE");
+        }
     }
+
 
     @AfterMethod
     static void tearDown() {
