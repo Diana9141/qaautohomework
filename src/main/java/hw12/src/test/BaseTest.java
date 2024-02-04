3c5e1631e99656a58a6d900c1b650f9f9f653665
diana <diana.braun@flexmade.com>
2024-02-04 23:38:32 +0200

    upd

--- src/main/java/hw12/src/test/BaseTest.java
+++ src/main/java/hw12/src/test/BaseTest.java
@@ -1,4 +1,31 @@
 package hw12.src.test;
 
+import com.browserup.bup.BrowserUpProxy;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import hw12.src.test.utils.DriverManager;
+import org.testng.annotations.BeforeMethod;
+
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
 public class BaseTest {
+    protected String testCaseName;
+    protected BrowserUpProxy proxy;
+
+    @BeforeMethod
+    public void setUp(Method method) {
+        this.testCaseName = method.getName();
+        DriverManager.init(testCaseName);
+        proxy = Objects.requireNonNull(WebDriverRunner.getSelenideProxy().getProxy());
+        clearBrowserCookies();
+        clearBrowserLocalStorage();
+
+        Configuration.baseUrl = "https//:www.saucedemo.com/";
+        Configuration.browserSize = "1920x1080";
+        open();
+    }
 }
--- src/main/java/hw12/src/test/HomeWorkTest.java
+++ src/main/java/hw12/src/test/HomeWorkTest.java
@@ -1,21 +1,10 @@
 package hw12.src.test;
 
-import com.codeborne.selenide.Configuration;
 import hw12.src.pages.LoginPage;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.open;
-
-public class HomeWorkTest {
+public class HomeWorkTest extends BaseTest {
     private final LoginPage loginPage = new LoginPage();
-
-    @BeforeMethod
-    static void setUp() {
-        Configuration.baseUrl = "https://www.saucedemo.com";
-        Configuration.browserSize = "1920x1080";
-        open("/");
-    }
 
     @Test
     void checkSuccessOrder() {
